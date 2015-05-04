386bb545f875a1fa259eab9585ec3ceb4b0c2e5c
Mann <chanmannlim@gmail.com>
2015-05-03 21:13:39 -0500

    add confusion table

--- +BN1/north_america.m
+++ +BN1/north_america.m
@@ -34,4 +34,13 @@ for i=1:length(ppp)
     predicted_ppp(i) = domain1(most_likely);
 end
 
+ppp_is_H = predicted_ppp(ppp == 'H');
+ppp_is_M = predicted_ppp(ppp == 'M');
+ppp_is_L = predicted_ppp(ppp == 'L');
+
+confusion_table(1, :) = [sum(ppp_is_H == 'H') sum(ppp_is_H == 'M') sum(ppp_is_H == 'L')];
+confusion_table(2, :) = [sum(ppp_is_M == 'H') sum(ppp_is_M == 'M') sum(ppp_is_M == 'L')];
+confusion_table(3, :) = [sum(ppp_is_L == 'H') sum(ppp_is_L == 'M') sum(ppp_is_L == 'L')];
+
+display(confusion_table);
 accuracy = sum(ppp == predicted_ppp) / sum(~isnan(ppp))
