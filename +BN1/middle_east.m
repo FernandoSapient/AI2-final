47e8a1b413fd0c4df4f974f4b527655d1afe9b38
Mann <chanmannlim@gmail.com>
2015-05-07 17:51:04 -0500

    extract make_factor and prediction_by_elimination

--- +BN1/middle_east.m
+++ +BN1/middle_east.m
@@ -12,58 +12,10 @@ bn.cpts{4} = P_Journal;
 domains = combination({domain' domain' domain' domain'});
 % joint factors
 factors = P_PPP_given_Journal_Agriculture_Industry_Government;
-[~, D] = size(e);
-
-for d = 1:D
-    column = domains(:, end-d+1);
-    for value = 'HML' + 0
-        factor = factors(column == value, :) * bn.cpts{d}(domain == value);
-        factors(column == value, :) = factor;
-    end
-end
+factors = make_factors(e, domains, bn, factors);
 
 % prediction
-predicted_ppp = zeros( size(ppp) );
-for k = 1:length(ppp)
-    probability = factors;
-    current_domains = domains;
-    
-    % loop for all variables in an evidence
-    for d = 1:D
-        variable = e(k, d);
-        column = current_domains(:, end);
-        
-        if isnan(variable)
-            [r, c] = size(probability);
-            sum_out = zeros(r/3, c);
-            
-            % Summing out
-            for l = 1:size(sum_out, 1)
-                from = (l-1) * 3 + 1;
-                to = from + 2;
-                sum_out(l, :) = sum( probability(from:to, :) );
-            end
-            probability = sum_out;
-        else
-            % Lookup when the variable exists
-            probability = probability(column == variable, :);
-        end
-        
-        % re-generate current_domains
-        variable_domains = cell(1, D-d);
-        for l = 1:D-d
-            variable_domains{l} = domain';
-        end
-
-        if size(variable_domains, 2) > 0
-            current_domains = combination(variable_domains);
-        end
-    end
-    
-    % prediction
-    [~, most_likely] = max(probability);
-    predicted_ppp(k) = domain(most_likely);
-end
+predicted_ppp = prediction_by_elimination(e, domains, factors);
 
 % confusion and accuracy
 confusion_table = confusion(ppp, predicted_ppp);
---
+++ prediction_by_elimination.m
@@ -0,0 +1,54 @@
+function [ prediction ] = prediction_by_elimination( e, domains, factors )
+% prediction_by_elimination - Predict PPP by elimination inference
+%
+% input:
+%       - e, the evidences
+%       - domains, the joint domains for all the evidences
+%       - factors, the joint factors
+
+% Assume domain = ['H', 'M', 'L']' for all discretized variables;
+domain = ['H' 'M' 'L']' + 0;
+
+[R, D] = size(e);
+prediction = zeros(R, 1);
+for k = 1:R
+    current_domains = domains;
+    probability = factors;
+    
+    % loop for all variables in an evidence
+    for d = 1:D
+        variable = e(k, d);
+        column = current_domains(:, end);
+        
+        if isnan(variable)
+            [r, c] = size(probability);
+            sum_out = zeros(r/3, c);
+            
+            % Summing out
+            for l = 1:size(sum_out, 1)
+                from = (l-1) * 3 + 1;
+                to = from + 2;
+                sum_out(l, :) = sum( probability(from:to, :) );
+            end
+            probability = sum_out;
+        else
+            % Lookup when the variable exists
+            probability = probability(column == variable, :);
+        end
+        
+        % re-generate current_domains
+        variable_domains = cell(1, D-d);
+        for l = 1:D-d
+            variable_domains{l} = domain';
+        end
+
+        if size(variable_domains, 2) > 0
+            current_domains = combination(variable_domains);
+        end
+    end
+    
+    % prediction
+    [~, most_likely] = max(probability);
+    prediction(k) = domain(most_likely);
+end
+
